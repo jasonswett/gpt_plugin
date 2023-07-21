b60e1ef94943103b7b7669575666a56c1c8e0c34
Jason Swett <jason@benfranklinlabs.com>
2023-07-20 21:09:41 -0400

    Add spacing and clarify test names.

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 from gpt_plugin import GptPlugin
 
-def test_parse_response_with_code():
+def test_parse_response_with_code_block():
     plugin = GptPlugin(None)
+
     response = """
     Here is some code:
     ```python
@@ -9,13 +10,16 @@ def test_parse_response_with_code():
     assert True
     ```
     """
+
     code = plugin.parse_response(response)
     assert code == "def test():\n    assert True"
 
-def test_parse_response_without_code():
+def test_parse_response_without_code_block():
     plugin = GptPlugin(None)
+
     response = """
     There is no code block present
     """
+
     code = plugin.parse_response(response)
     assert code is None
