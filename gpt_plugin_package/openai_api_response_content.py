57fff05d34dc127abf45a1ce13d626c9767d678c
Jason Swett <jason@benfranklinlabs.com>
2023-07-21 22:04:42 -0400

    Add filename extraction.

--- gpt_plugin_package/openai_api_response_content.py
+++ gpt_plugin_package/openai_api_response_content.py
@@ -4,6 +4,12 @@ class OpenAIAPIResponseContent:
     def __init__(self, body):
         self.body = body
 
+    def filename(self):
+        filename_content = self.body.split('\n', 1)
+        if len(filename_content) < 1:
+            return None
+        return filename_content[0].strip()
+
     def code_block(self):
         filename_content = self.body.split('\n', 1)
 
--- gpt_plugin_package/tests.py
+++ gpt_plugin_package/tests.py
@@ -9,3 +9,10 @@ def test_code_block_with_code_block():
     content = OpenAIAPIResponseContent(body)
     code = content.code_block()
     assert code == "RSpec.describe \"stuff\" do\nend"
+
+def test_filename_extraction():
+    body = "test_spec.rb\n\n```ruby\nRSpec.describe \"stuff\" do\nend\n```"
+
+    content = OpenAIAPIResponseContent(body)
+    filename = content.filename()
+    assert filename == "test_spec.rb"
