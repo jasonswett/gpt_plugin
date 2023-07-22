46663b3cf9e5eb94e81674b4d53cb97047fafa4f
Jason Swett <jason@benfranklinlabs.com>
2023-07-21 22:05:58 -0400

    Remove superfluous code.

--- gpt_plugin_package/openai_api_response_content.py
+++ gpt_plugin_package/openai_api_response_content.py
@@ -11,14 +11,5 @@ class OpenAIAPIResponseContent:
         return filename_content[0].strip()
 
     def code_block(self):
-        filename_content = self.body.split('\n', 1)
-
-        if len(filename_content) < 2:
-            return None
-
-        code_content = filename_content[1].strip()
-
-        # Remove code fences
-        code_content = re.sub(r'```.*$', '', code_content, flags=re.MULTILINE)  # remove the opening code fence
-        code_content = code_content.rstrip("`")  # remove the closing code fence
-        return code_content.strip()  # return the content part after stripping the leading and trailing whitespaces
+        code_content = self.body.split('\n', 1)[1]
+        return re.sub(r'```.*$', '', code_content, flags=re.MULTILINE).rstrip("`").strip()
