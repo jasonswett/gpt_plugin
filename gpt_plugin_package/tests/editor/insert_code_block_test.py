f59b9a1b98b877bc89c96a7da1e62fffd22b4595
Jason Swett <jason@benfranklinlabs.com>
2023-09-17 10:01:14 -0400

    Add tests.

--- gpt_plugin_package/tests/editor/insert_code_block_test.py
+++ gpt_plugin_package/tests/editor/insert_code_block_test.py
@@ -14,20 +14,52 @@ def nvim():
     for buffer in nvim.buffers:
         nvim.command(f'bd! {buffer.number}')  # bd! is a command to delete a buffer
 
-def test_insert_code_block(nvim):
+# Scenarios:
+#
+# GPT gives us a filename and some code associated with that filename
+#
+# The current buffer is the only buffer
+#   The current buffer is empty
+#   The current buffer is not empty
+#
+# The current buffer is not the only buffer
+#   The target filename matches the current buffer
+#   The target filename does not match the current buffer
+
+def test_current_buffer_is_only_buffer_and_current_buffer_is_empty(nvim):
     directory = '.'
     editor = Editor(nvim, directory)
-    file_path = os.path.join(directory, "test_code.rb")
-    code_block = "def hello():\n    print('Hello, world!')"
+    file_path = os.path.join(directory, "martin.rb")
+    code_block = "print('hi')"
 
-    editor.insert_code_block("test_code.rb", code_block)
-
-    with open(file_path, 'r') as f:
-        # Expect an extra newline character at the end of the file
-        assert f.read() == code_block + '\n'
+    editor.insert_code_block("martin.rb", code_block)
 
     nvim.command(f"edit {file_path}")
-    assert editor.current_filename() == "test_code.rb"
+    assert editor.current_filename() == "martin.rb"
 
-    # If the file needs to be removed after the test
+    os.remove(file_path)
+
+def test_current_buffer_is_only_buffer_and_current_buffer_is_not_empty(nvim):
+    # Setup
+    directory = '.'
+    editor = Editor(nvim, directory)
+
+    # Create a non-empty buffer
+    non_empty_content = "initial content"
+    nvim.current.buffer[:] = [non_empty_content]
+
+    file_name = "martin.rb"
+    file_path = os.path.join(directory, file_name)
+    code_block = "print('hi')"
+
+    # Action
+    editor.insert_code_block(file_name, code_block)
+
+    # Verification
+    # Ensure the tab with the specified filename has the inserted code block
+    nvim.command(f"tabnext | edit {file_path}")  # Switch to the tab and open the file
+    assert editor.current_filename() == file_name
+    assert "\n".join(nvim.current.buffer[:]) == code_block
+
+    # Cleanup
     os.remove(file_path)
