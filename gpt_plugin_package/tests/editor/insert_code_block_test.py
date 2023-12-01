5ddc90458881e8152e0ea86b1f1660d6d2049016
Jason Swett <jason@benfranklinlabs.com>
2023-11-30 21:31:43 -0500

    Clean up tests.

--- gpt_plugin_package/tests/editor/insert_code_block_test.py
+++ gpt_plugin_package/tests/editor/insert_code_block_test.py
@@ -19,33 +19,48 @@ class TestCurrentBufferIsOnlyBuffer:
         self.directory = '.'
         self.file_name = "martin.rb"
         self.file_path = os.path.join(self.directory, self.file_name)
-        self.code_block = "print('hi')"
 
     def teardown_method(self):
         if os.path.exists(self.file_path):
             os.remove(self.file_path)
 
-    def test_current_buffer_is_empty(self, nvim):
+    def test_1(self, nvim):
+        # When the current buffer is empty
         editor = Editor(nvim, self.directory)
-        editor.insert_code_block(self.file_name, self.code_block)
-        nvim.command(f"edit {self.file_path}")
-        assert editor.current_filename() == self.file_name
 
-    def test_current_buffer_is_not_empty_expected_contents(self, nvim):
+        # And we insert a code block for
+        # a new file called martin.rb
+        editor.insert_code_block("martin.rb", "puts 'hello world'")
+
+        # Then the result is that the current buffer is martin.rb
+        assert editor.current_filename() == "martin.rb"
+
+    def test_2(self, nvim):
+        # When the current buffer is not empty
         editor = Editor(nvim, self.directory)
         nvim.current.buffer[:] = ["initial content"]
-        editor.insert_code_block(self.file_name, self.code_block)
-        nvim.command(f"tabnext | edit {self.file_path}")
-        assert editor.current_filename() == self.file_name
-        assert "\n".join(nvim.current.buffer[:]) == self.code_block
 
-    def test_current_buffer_is_not_empty_original_content_undisturbed(self, nvim):
+        # And we insert a code block for
+        # a new file called martin.rb
+        editor.insert_code_block("martin.rb", "puts 'hello world'")
+
+        # The content of the martin.rb buffer gets replaced
+        # with the inserted code block
+        nvim.command(f"edit martin.rb")
+        assert "\n".join(nvim.current.buffer[:]) == "puts 'hello world'"
+
+    def test_3(self, nvim):
         editor = Editor(nvim, self.directory)
-        jason_file_name = "jason.rb"
-        jason_content = "i am jason"
-        nvim.current.buffer[:] = [jason_content]
-        nvim.command(f'w {jason_file_name}')
-        editor.insert_code_block(self.file_name, self.code_block)
-        nvim.command(f'buffer {jason_file_name}')
-        assert "\n".join(nvim.current.buffer[:]) == jason_content
-        os.remove(os.path.join(self.directory, jason_file_name))
+
+        # When some content exists in some tab
+        nvim.current.buffer[:] = ["i am jason"]
+        nvim.command(f'w jason.rb')
+
+        # And we insert a code block into a different tab
+        editor.insert_code_block("martin.rb", "puts 'hello world'")
+
+        # The original tab is untouched
+        nvim.command(f'buffer jason.rb')
+        assert "\n".join(nvim.current.buffer[:]) == "i am jason"
+
+        os.remove(os.path.join(self.directory, "jason.rb"))
