a2af949a790303c9a125008698c893def757d456
Jason Swett <jason@benfranklinlabs.com>
2023-12-10 09:44:34 -0500

    Refactoring.

--- gpt_plugin_package/editor.py
+++ gpt_plugin_package/editor.py
@@ -9,18 +9,19 @@ class Editor:
         return os.path.relpath(self.nvim.current.buffer.name, self.directory)
 
     def insert_code_block(self, filename, code_block):
-        if code_block:
-            if self.is_buffer_with_filename_open(filename):
-                self.nvim.command(f'buffer {filename}')
-            else:
-                if not self.is_current_buffer_empty():
-                    self.nvim.command('tabnew')
-
-            self.nvim.current.buffer[:] = code_block.split('\n')
-            path = os.path.join(self.directory, filename)
-            self.save_file(path)
-        else:
+        if not code_block:
             self.nvim.current.buffer[:] = ["No code found in response"]
+            return
+
+        if self.is_buffer_with_filename_open(filename):
+            self.nvim.command(f'buffer {filename}')
+        else:
+            if not self.is_current_buffer_empty():
+                self.nvim.command('tabnew')
+
+        self.nvim.current.buffer[:] = code_block.split('\n')
+        path = os.path.join(self.directory, filename)
+        self.save_file(path)
 
     def is_buffer_with_filename_open(self, filename):
         all_open_buffers = self.nvim.buffers
